e9e050951098fc47b75843a9f279f999ccb4d9f5
Yogesh Narang <yogesh.narang49@gmail.com>
2019-12-16 20:08:03 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line
+import React from 'react'
+import Table from './components/Table'
+import Header from './components/Header'
+
+const App = () =>
+  (<div>
+    <div>
+      <Header />
+    </div>  
+      <div style={{ width: 'max-content' }}>
+        <Table x={100} y={100} id={'1'} />
+      </div>
+    </div>)
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class Header extends React.Component{
+    render(){
+
+        const styleContainer = {
+            display:"felx",
+            color: "white",
+            backgroundColor: "#808080",
+            padding: "10px",
+            fontFamily: "Arial",
+            textAlign: "center",
+            //borderWidth: "10px",
+            borderStyle: "solid",
+            borderRadius:'5px!important',
+            
+            // borderColor:"black",
+        };
+        const styleOptionsBar = {
+            display:"felx",
+            color: "white",
+            backgroundColor: "#808080",
+            padding: "0px",
+            fontFamily: "Arial",
+            textAlign: "center",
+            //borderWidth: "10px",
+            borderStyle: "solid",
+            borderRadius:'5px!important',
+            
+            // borderColor:"black",
+        };
+        const stylecomponent ={
+            // height: "20px",
+            //width: "20px",
+            //backgroundColor:"DarkBlue",
+            //padding:"5px",
+            textAlign:"center",
+            textColor:"#808080",
+            boxSizing: "border-box",
+            border:"10px black",
+        };
+        return(
+            <div>
+                <h1 style = {{paddingLeft:"10px"}}>ShareDocs Testing Phase 1 </h1>
+
+                <div style={styleOptionsBar} className="OptionsBar-Container">
+                    <h4>
+                        OptionsBarBuild in progress    
+                    </h4>
+                </div>
+
+                <div style={styleContainer} className="ToolBar-Container">
+
+                    <div style ={stylecomponent} className="ToolBar-Component">
+                        <h3>ToolBar building in progress</h3>
+                    </div>
+                   
+                    </div>
+            </div>
+        )
+    }
+}
